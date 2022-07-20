895dc75d92629f325b037570a3131d42309189a1
Karc <kevinrojascobenas@gmail.com>
2022-07-20 10:29:29 -0500

    Cambios

--- openfeign/src/main/java/com/libreapp/nttdata/validar/producto/ProductoCheckClient.java
+++ openfeign/src/main/java/com/libreapp/nttdata/validar/producto/ProductoCheckClient.java
@@ -1,7 +1,11 @@
 package com.libreapp.nttdata.validar.producto;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient("validarproducto")
 public interface ProductoCheckClient {
+    @GetMapping(path = "api/v1/product-check")
+    public ProductoCheckResponse isRepeat(@RequestParam(name = "id") long id, @RequestParam(name = "serie") String serie);
 }
--- validarproducto/src/main/java/com/libreapp/nttdata/validar/producto/ValidarProductoApplication.java
+++ validarproducto/src/main/java/com/libreapp/nttdata/validar/producto/ValidarProductoApplication.java
@@ -2,7 +2,9 @@ package com.libreapp.nttdata.validar.producto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class ValidarProductoApplication {
     public static void main(String[] args) {
